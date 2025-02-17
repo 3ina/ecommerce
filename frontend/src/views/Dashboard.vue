bf86076cf32e8a750cbb6fc479e50724aa92dc90
sinaroydel <3inaroydl@gmail.com>
2025-02-17 13:40:53 +0330

    add dashboard view to front

---
+++ frontend/src/views/Dashboard.vue
@@ -0,0 +1,11 @@
+<template>
+    <HelloWorld msg="hello dashboars"></HelloWorld>
+</template>
+
+<script setup>
+    import HelloWorld from "../components/HelloWorld.vue";
+</script>
+
+<style scoped>
+
+</style>
