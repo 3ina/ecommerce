64bc901073fc4806d9c82420ccada9c2fe16524f
sinaroydel <3inaroydl@gmail.com>
2025-02-17 14:57:43 +0330

    add component  ResetPassword

--- frontend/src/views/RequestPasswordReset.vue
+++ frontend/src/views/RequestPasswordReset.vue
@@ -10,7 +10,7 @@
                 <div>
                     <label for="email" class="block text-sm/6 font-medium text-gray-900">Email address</label>
                     <div class="mt-2">
-                        <input type="email" name="email" id="email" autocomplete="email" required="" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+                        <input type="email" name="email" id="email" autocomplete="email" required="" class="block w-full  bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
                     </div>
                 </div>
                 <div class="flex items-center justify-between">
@@ -21,7 +21,9 @@
 
 
                 <div>
-                    <button type="submit" class="flex w-full justify-center rounded-md !bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
+                    <button type="submit" class="flex w-full justify-center rounded-md !bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                        Submit
+                    </button>
                 </div>
             </form>
 
