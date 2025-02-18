8c47ffae8d1dfe3a82a6b727cfe6634a8fca382c
sinaroydel <3inaroydl@gmail.com>
2025-02-18 10:29:20 +0330

    add app layout

---
+++ frontend/src/components/AppLayout.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="flex min-h-full">
+        <!--   Side bar -->
+        <sidebar/>
+        <!--/   Side bar -->
+
+
+        <div>
+            <header class="h-8 shadow bg-white">
+                Header
+            </header>
+
+            <!--Content -->
+
+            <main>
+                <router-view>
+                </router-view>
+            </main>
+            <!--/   content -->
+
+        </div>
+    </div>
+</template>
+
+
+<script setup>
+    import Sidebar from "./Sidebar.vue";
+
+    const {title} = defineProps({
+        title: String,
+    })
+
+    const emit = defineEmits(['submit'])
+</script>
+
+
+<style scoped>
+
+</style>
