4ac0cce3a0c14e852cbd4367ada0d45ce421496b
sinaroydel <3inaroydl@gmail.com>
2025-02-19 13:36:27 +0330

    update applayout

--- frontend/src/components/AppLayout.vue
+++ frontend/src/components/AppLayout.vue
@@ -1,30 +1,29 @@
 <template>
-    <div class="flex min-h-full">
+    <div class="flex min-h-full min-w-screen">
         <!--   Side bar -->
         <sidebar/>
         <!--/   Side bar -->
-
-
-        <div>
-            <header class="h-8 shadow bg-white">
-                Header
-            </header>
+        <div class="flex-1">
+            <TopHeader/>
 
             <!--Content -->
-
-            <main>
-                <router-view>
-                </router-view>
-            </main>
+                <main>
+                    <div class="p-6">
+                        <router-view></router-view>
+                    </div>
+                </main>
             <!--/   content -->
-
         </div>
+
+
+
     </div>
 </template>
 
 
 <script setup>
     import Sidebar from "./Sidebar.vue";
+    import TopHeader from "./TopHeader.vue";
 
     const {title} = defineProps({
         title: String,
