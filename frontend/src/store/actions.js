c8545304dfbde52a5b72c896431abd173fc18491
sinaroydel <3inaroydl@gmail.com>
2025-02-26 22:29:00 +0330

    add logout action

--- frontend/src/store/actions.js
+++ frontend/src/store/actions.js
@@ -8,3 +8,13 @@ export function login({commit},data) {
             return response;
     })
 }
+
+
+export function logout({commit}) {
+    return axios.post("logout").
+    then(response => {
+        commit("setUser", null);
+        commit("setToken", null);
+        return response;
+    })
+}
