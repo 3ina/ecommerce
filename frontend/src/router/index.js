9c418d94aa6e401b8898670f508762dc61063909
sinaroydel <3inaroydl@gmail.com>
2025-02-26 22:06:31 +0330

    add auth require for some routes

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,10 +1,14 @@
 import {createRouter,createWebHistory} from "vue-router";
+import store from "../store/index.js";
 
 const routes = [
     {
         path: '/app',
         name: 'app',
         component: () => import('../components/AppLayout.vue'),
+        meta: {
+            requiresAuth: true,
+        },
         children: [
             {
                 path: 'dashboard',
@@ -22,22 +26,48 @@ const routes = [
         path: '/login',
         name: 'login',
         component: () => import('../views/Login.vue'),
+        meta: {
+            requiresGuest: true,
+        }
     },
     {
         path: '/request-password',
         name: 'requestPassword',
         component: () => import('../views/RequestPasswordReset.vue'),
+        meta: {
+            requiresGuest: true,
+        }
     },
     {
         path: '/reset-password/:token',
         name: 'ResetPassword',
         component: () => import('../views/ResetPassword.vue'),
+        meta: {
+            requiresGuest: true,
+        }
     },
+    {
+        path: '/:pathMatch(.*)',
+        name: '404',
+        component: () => import('../views/404.vue'),
+
+    }
 ];
 
 const router = createRouter({
     history: createWebHistory(),
     routes,
 });
+
+router.beforeEach((to, from, next) => {
+    if (to.meta.requiresAuth && !store.state.user.token) {
+        next({name: 'login'});
+    }else if (to.meta.requiresGuest && store.state.user.token) {
+       next({name: 'app.dashboard'});
+    }
+    else {
+        next();
+    }
+})
 
 export default router;
